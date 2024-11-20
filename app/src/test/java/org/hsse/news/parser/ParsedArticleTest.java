e5c9820eb1c8ca451e5ad785332d41a210c78119
Vlad_Ali <marknext592@gmail.com>
2024-11-20 17:51:59 +0300

    Update 2 RssParser

---
+++ app/src/test/java/org/hsse/news/parser/ParsedArticleTest.java
@@ -0,0 +1,31 @@
+package org.hsse.news.parser;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParsedArticleTest {
+    String testString = "Website: StackOverflow\n" +
+            "name: Meet the guy\n" +
+            "author: Eira May\n" +
+            "topics: [gaming]\n" +
+            "date: 15 ноября 2024\n" +
+            "description: Chris Fowler, Director of Engine for Call of Duty.  https://stackoverflow.blog/2024/11/15/meet-the-guy-responsible-for-building-the-call-of-duty-game-engine/";
+    ParsedArticle testArticle = new ParsedArticle("Meet the guy", "Chris Fowler, Director of Engine for Call of Duty.", "15 ноября 2024", "https://stackoverflow.blog/2024/11/15/meet-the-guy-responsible-for-building-the-call-of-duty-game-engine/", Set.of("gaming"),"Eira May", "StackOverflow");
+    @Test
+    void testRecord(){
+        assertEquals(testArticle.name(), "Meet the guy");
+        assertEquals(testArticle.description(),"Chris Fowler, Director of Engine for Call of Duty.");
+        assertEquals(testArticle.date(), "15 ноября 2024");
+        assertEquals(testArticle.link(), "https://stackoverflow.blog/2024/11/15/meet-the-guy-responsible-for-building-the-call-of-duty-game-engine/");
+        assertEquals(testArticle.topics(), Set.of("gaming"));
+        assertEquals(testArticle.author(), "Eira May");
+        assertEquals(testArticle.articleTag(),"StackOverflow");
+    }
+    @Test
+    void testtoString(){
+        assertEquals(testString,testArticle.toString());
+    }
+}
