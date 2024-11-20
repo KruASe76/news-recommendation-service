2ee0f282784c4b1c871e64cf95e7bef7600233e8
Vlad_Ali <marknext592@gmail.com>
2024-11-20 17:51:59 +0300

    Update 2 RssParser

---
+++ app/src/main/java/org/hsse/news/parser/ParsedArticle.java
@@ -0,0 +1,13 @@
+package org.hsse.news.parser;
+
+import java.util.Set;
+
+public record ParsedArticle(String name, String description, String date, String link, Set<String> topics, String author, String articleTag) {
+
+    @Override
+    public String toString() {
+        return "Website: " + articleTag + "\n" + "name: " + name +
+                "\n" + "author: " + author + "\n" + "topics: " + topics + "\n" +
+                "date: " + date + "\n" + "description: " + description + "  " + link;
+    }
+}
