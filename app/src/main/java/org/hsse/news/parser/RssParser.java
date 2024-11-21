834453ccdd6de01fb0885a8236b5ac04bdb6f7d8
Vlad_Ali <marknext592@gmail.com>
2024-11-21 22:46:49 +0300

    Update 3 RssParser

--- app/src/main/java/org/hsse/news/parser/RssParser.java
+++ app/src/main/java/org/hsse/news/parser/RssParser.java
@@ -6,7 +6,6 @@ import com.rometools.rome.feed.synd.SyndFeed;
 import com.rometools.rome.io.FeedException;
 import com.rometools.rome.io.SyndFeedInput;
 
-
 import java.io.File;
 import java.io.IOException;
 import java.net.HttpURLConnection;
@@ -20,46 +19,73 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-
 import com.rometools.rome.io.XmlReader;
 import org.jsoup.Jsoup;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class RssParser {
-    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd MMMM yyyy");
-    public static List<ParsedArticle> parse(URL url, String articleTag) throws IOException, FeedException {
-        final HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestProperty("User-agent", "Mozilla/5.0");
-        final XmlReader reader = new XmlReader(connection.getInputStream(), true, UTF_8.name());
-        return parse(reader, articleTag);
-    }
-    public static List<ParsedArticle> parse (File file, String articleTag) throws IOException, FeedException {
-        final XmlReader reader = new XmlReader(file);
-        return parse(reader, articleTag);
-    }
-    private static List<ParsedArticle> parse(XmlReader reader, String  articleTag) throws FeedException {
-        final List<ParsedArticle> articles = new ArrayList<>();
-        final SyndFeedInput syndFeedInput = new SyndFeedInput();
-        syndFeedInput.setAllowDoctypes(true);
-        final SyndFeed feed = syndFeedInput.build(reader);
-        for (SyndEntry entry : feed.getEntries()) {
-            final String name = entry.getTitle();
-            final String description = Jsoup.parse(entry.getDescription().getValue()).text();
-            final Instant date = entry.getPublishedDate().toInstant();
-            final String link = entry.getLink();
-            final Set<String> topics = getTopics(entry.getCategories());
-            final String author = entry.getAuthor();
-            articles.add(new ParsedArticle(name, description, LocalDate.ofInstant(date, ZoneId.systemDefault()).format(DATE_FORMAT), link, topics, author, articleTag));
-        }
-        return articles;
-    }
+  private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd MMMM yyyy");
 
-    private static Set<String> getTopics(List<SyndCategory> categoryList) {
-        Set<String> categories = new HashSet<>();
-        for (SyndCategory category : categoryList) {
-            categories.add(Jsoup.parse(category.getName()).text());
-        }
-        return categories;
+  public static List<ParsedArticle> parse(URL url, String articleTag)
+      throws IOException, FeedException {
+    final HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+    connection.setRequestProperty("User-agent", "Mozilla/5.0");
+    final XmlReader reader = new XmlReader(connection.getInputStream(), true, UTF_8.name());
+    try {
+      List<ParsedArticle> articles = parse(reader, articleTag);
+      return articles;
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    } finally {
+      reader.close();
     }
+  }
+
+  public static List<ParsedArticle> parse(File file, String articleTag)
+      throws IOException, FeedException {
+    XmlReader reader = new XmlReader(file);
+    try {
+      List<ParsedArticle> articles = parse(reader, articleTag);
+      return articles;
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    } finally {
+      reader.close();
+    }
+  }
+
+  private static List<ParsedArticle> parse(XmlReader reader, String articleTag)
+      throws FeedException, IOException {
+    final List<ParsedArticle> articles = new ArrayList<>();
+    final SyndFeedInput syndFeedInput = new SyndFeedInput();
+    syndFeedInput.setAllowDoctypes(true);
+    final SyndFeed feed = syndFeedInput.build(reader);
+    for (SyndEntry entry : feed.getEntries()) {
+      final String name = entry.getTitle();
+      final String description = Jsoup.parse(entry.getDescription().getValue()).text();
+      final Instant date = entry.getPublishedDate().toInstant();
+      final String link = entry.getLink();
+      final Set<String> topics = getTopics(entry.getCategories());
+      final String author = entry.getAuthor();
+      articles.add(
+          new ParsedArticle(
+              name,
+              description,
+              LocalDate.ofInstant(date, ZoneId.systemDefault()).format(DATE_FORMAT),
+              link,
+              topics,
+              author,
+              articleTag));
+    }
+    return articles;
+  }
+
+  private static Set<String> getTopics(List<SyndCategory> categoryList) {
+    Set<String> categories = new HashSet<>();
+    for (SyndCategory category : categoryList) {
+      categories.add(Jsoup.parse(category.getName()).text());
+    }
+    return categories;
+  }
 }
--- app/src/test/java/org/hsse/news/parser/RssParserTest.java
+++ app/src/test/java/org/hsse/news/parser/RssParserTest.java
@@ -12,28 +12,35 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RssParserTest {
-    ClassLoader classLoader = this.getClass().getClassLoader();
+  @Test
+  void shouldParseFile() throws IOException, FeedException {
+    ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
     File testFile = new File(classLoader.getResource("RssTestFile.xml").getFile());
+    String testString =
+        "Website: StackOverflow\n"
+            + "name: Meet the guy responsible for building the Call of Duty game engine\n"
+            + "author: Eira May\n"
+            + "topics: [gaming, se-stackoverflow, game-developer, se-tech, podcast, game-development]\n"
+            + "date: 15 ноября 2024\n"
+            + "description: Chris Fowler, Director of Engine for Call of Duty, tells Ben and Ryan about his path from marine biology to game development, the ins and outs of game engines, and the technical feats involved in creating massively popular games like Call of Duty. Chris also explains why community feedback is so critical in game development and offers his advice for aspiring game developers.  https://stackoverflow.blog/2024/11/15/meet-the-guy-responsible-for-building-the-call-of-duty-game-engine/";
+    List<ParsedArticle> articles = RssParser.parse(testFile, "StackOverflow");
+    assertEquals(articles.get(0).toString(), testString);
+  }
 
-    String testString = "Website: StackOverflow\n" +
-            "name: Meet the guy responsible for building the Call of Duty game engine\n" +
-            "author: Eira May\n" +
-            "topics: [gaming, se-stackoverflow, game-developer, se-tech, podcast, game-development]\n" +
-            "date: 15 ноября 2024\n" +
-            "description: Chris Fowler, Director of Engine for Call of Duty, tells Ben and Ryan about his path from marine biology to game development, the ins and outs of game engines, and the technical feats involved in creating massively popular games like Call of Duty. Chris also explains why community feedback is so critical in game development and offers his advice for aspiring game developers.  https://stackoverflow.blog/2024/11/15/meet-the-guy-responsible-for-building-the-call-of-duty-game-engine/";
-    @Test
-    void ParseTest() throws IOException, FeedException {
-        List<ParsedArticle> articles = RssParser.parse(testFile,"StackOverflow");
-        assertEquals(articles.get(0).toString(),testString);
-    }
+  @Test
+  void shouldParseSite() throws IOException, FeedException {
+    List<ParsedArticle> articles =
+        RssParser.parse(new URL("https://stackoverflow.blog/feed/"), "StackOverflow");
+    assertEquals(articles.get(0).articleTag(), "StackOverflow");
+  }
 
-    @Test
-    void shouldThrowFileNotFoundException(){
-        assertThrows(FileNotFoundException.class, ()->RssParser.parse(new File("1"),"1"));
-    }
+  @Test
+  void shouldThrowFileNotFoundException() {
+    assertThrows(FileNotFoundException.class, () -> RssParser.parse(new File("1"), "1"));
+  }
 
-    @Test
-    void shouldThrowIOException(){
-        assertThrows(IOException.class, ()->RssParser.parse(new URL("1"),"1"));
-    }
+  @Test
+  void shouldThrowIOException() {
+    assertThrows(IOException.class, () -> RssParser.parse(new URL("1"), "1"));
+  }
 }
