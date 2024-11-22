b773b67d351f73ca1bdd0d953be4cc0dc667a530
Vlad_Ali <marknext592@gmail.com>
2024-11-22 22:39:33 +0300

    Update 5 RssParser

--- app/src/main/java/org/hsse/news/parser/RssParser.java
+++ app/src/main/java/org/hsse/news/parser/RssParser.java
@@ -1,6 +1,7 @@
 package org.hsse.news.parser;
 
 import com.rometools.rome.feed.synd.SyndCategory;
+import com.rometools.rome.feed.synd.SyndContent;
 import com.rometools.rome.feed.synd.SyndEntry;
 import com.rometools.rome.feed.synd.SyndFeed;
 import com.rometools.rome.io.FeedException;
@@ -14,57 +15,50 @@ import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 import com.rometools.rome.io.XmlReader;
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
-public class RssParser {
+public final class RssParser {
   private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd MMMM yyyy");
+  private static final ZoneId ZONE_ID = ZoneId.systemDefault();
 
-  public static List<ParsedArticle> parse(URL url, String articleTag)
+  private RssParser() {}
+
+  public static List<ParsedArticle> parse(final URL url, final String articleTag)
       throws IOException, FeedException {
     final HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-    connection.setRequestProperty("User-agent", "Mozilla/5.0");
-    final XmlReader reader = new XmlReader(connection.getInputStream(), true, UTF_8.name());
-    try {
-      List<ParsedArticle> articles = parse(reader, articleTag);
-      return articles;
-    } catch (Exception e) {
-      throw new RuntimeException(e);
-    } finally {
+    setConnection(connection);
+    try (XmlReader reader = new XmlReader(connection.getInputStream(), true, UTF_8.name())) {
+      final List<ParsedArticle> articles = parse(reader, articleTag);
       reader.close();
+      return articles;
     }
   }
 
-  public static List<ParsedArticle> parse(File file, String articleTag)
+  public static List<ParsedArticle> parse(final File file, final String articleTag)
       throws IOException, FeedException {
-    XmlReader reader = new XmlReader(file);
-    try {
-      List<ParsedArticle> articles = parse(reader, articleTag);
-      return articles;
-    } catch (Exception e) {
-      throw new RuntimeException(e);
-    } finally {
+    try (XmlReader reader = new XmlReader(file)) {
+      final List<ParsedArticle> articles = parse(reader, articleTag);
       reader.close();
+      return articles;
     }
   }
 
-  private static List<ParsedArticle> parse(XmlReader reader, String articleTag)
-      throws FeedException, IOException {
+  private static List<ParsedArticle> parse(final XmlReader reader, final String articleTag)
+      throws FeedException {
     final List<ParsedArticle> articles = new ArrayList<>();
     final SyndFeedInput syndFeedInput = new SyndFeedInput();
     syndFeedInput.setAllowDoctypes(true);
     final SyndFeed feed = syndFeedInput.build(reader);
-    for (SyndEntry entry : feed.getEntries()) {
+    for (final SyndEntry entry : feed.getEntries()) {
       final String name = entry.getTitle();
-      final String description = Jsoup.parse(entry.getDescription().getValue()).text();
-      final Instant date = entry.getPublishedDate().toInstant();
+      final String description = getDescription(entry);
+      final Instant date = toInstantDate(entry.getPublishedDate());
       final String link = entry.getLink();
       final Set<String> topics = getTopics(entry.getCategories());
       final String author = entry.getAuthor();
@@ -72,7 +66,7 @@ public class RssParser {
           new ParsedArticle(
               name,
               description,
-              LocalDate.ofInstant(date, ZoneId.systemDefault()).format(DATE_FORMAT),
+              formatDate(LocalDate.ofInstant(date, ZONE_ID)),
               link,
               topics,
               author,
@@ -81,11 +75,45 @@ public class RssParser {
     return articles;
   }
 
-  private static Set<String> getTopics(List<SyndCategory> categoryList) {
-    Set<String> categories = new HashSet<>();
-    for (SyndCategory category : categoryList) {
-      categories.add(Jsoup.parse(category.getName()).text());
+  private static String formatDate(final LocalDate localDate) {
+    return localDate.format(DATE_FORMAT);
+  }
+
+  private static void setConnection(final HttpURLConnection connection) {
+    connection.setRequestProperty("User-agent", "Mozilla/5.0");
+  }
+
+  private static Set<String> getTopics(final List<SyndCategory> categoryList) {
+    final Set<String> categories = new HashSet<>();
+    for (final SyndCategory category : categoryList) {
+      final String categoryName = category.getName();
+      final Document document = parseText(categoryName);
+
+      categories.add(parseDocument(document));
     }
     return categories;
+  }
+
+  private static Instant toInstantDate(final Date date) {
+    return date.toInstant();
+  }
+
+  private static String getDescription(final SyndEntry entry) {
+    final SyndContent content = entry.getDescription();
+    return parseDescription(content);
+  }
+
+  private static Document parseText(final String text) {
+    return Jsoup.parse(text);
+  }
+
+  private static String parseDocument(final Document document) {
+    return document.text();
+  }
+
+  private static String parseDescription(final SyndContent content) {
+    final String text = content.getValue();
+    final Document document = parseText(text);
+    return parseDocument(document);
   }
 }
