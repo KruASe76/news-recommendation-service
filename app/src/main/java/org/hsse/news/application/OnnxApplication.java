f6d2dffda36d7e75744fb33d91646f8b3ca8c3ed
Чукалов Алексей <104710901+topki4uk@users.noreply.github.com>
2025-01-16 18:48:03 +0300

    delete onnx-code from application

--- app/src/main/java/org/hsse/news/application/OnnxApplication.java
+++ app/src/main/java/org/hsse/news/application/OnnxApplication.java
@@ -10,13 +10,13 @@ import org.hsse.news.model.OnnxModelRunner;
 import org.hsse.news.util.ResourceUtil;
 
 public class OnnxApplication {
-  private static final String MODEL_PATH = ResourceUtil.getResource("/onnx_model/trfs-model.onnx");
-  private static final String TOKENIZER_PATH = ResourceUtil.getResource("/onnx_model/tokenizer/tokenizer.json");
+  public static final String MODEL_PATH = ResourceUtil.getResource("/onnx_model/trfs-model.onnx");
+  public static final String TOKENIZER_PATH = ResourceUtil.getResource("/onnx_model/tokenizer/tokenizer.json");
   private final OnnxModelRunner modelRunner;
 
 
-  public OnnxApplication() throws IOException, OrtException {
-    modelRunner = new OnnxModelRunner(OnnxApplication.MODEL_PATH, OnnxApplication.TOKENIZER_PATH);
+  public OnnxApplication(OnnxModelRunner modelRunner) {
+    this.modelRunner = modelRunner;
   }
 
   private Map<String, Float> getResult(String text, List<String> labels) throws OrtException {
