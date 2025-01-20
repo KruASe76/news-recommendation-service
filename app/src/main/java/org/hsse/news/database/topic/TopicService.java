b1ddb8b5a1a2e302c5acbae39063186f99061fe6
Чукалов Алексей <104710901+topki4uk@users.noreply.github.com>
2025-01-21 02:16:58 +0300

    api: articles (#41)

--- app/src/main/java/org/hsse/news/database/topic/TopicService.java
+++ app/src/main/java/org/hsse/news/database/topic/TopicService.java
@@ -1,3 +1,9 @@
 package org.hsse.news.database.topic;
 
-public final class TopicService {}
+import org.hsse.news.database.topic.models.TopicId;
+
+public final class TopicService {
+    public String getTopicNameById(final TopicId id) {
+        return id.toString();
+    }
+}
